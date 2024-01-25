f7a6a186b470b83f6760fc77696238f476280304
Haytam LAZIZI-VIX <hlazizi1@numerique-educatif.fr>
2024-01-25 16:08:34 +0100

    Ajoutez des fichiers projet.

---
+++ Humain.cpp
@@ -0,0 +1,33 @@
+#include "Humain.h"
+#include <iostream>
+using namespace std;
+Humain::Humain(const string _nom, const string _boissonFavorite)
+{
+	nom = _nom;
+	boissonFavorite = _boissonFavorite;
+}
+
+void Humain::sePresente()
+{
+	cout << " Bonjour je m'appelle " << getNom() << " et ma boisson favorite est "<< getBoissonFavorite() << " GLOUPS!" << endl;
+}
+
+void Humain::setBoissonFavorite(const string bF)
+{
+	boissonFavorite = bF;
+}
+
+void Humain::boit()
+{
+	cout << " GLOUPS ! "<<endl;
+}
+
+string Humain::getNom()
+{
+	return nom;
+}
+
+string Humain::getBoissonFavorite()
+{
+	return boissonFavorite;
+}
---
+++ Humain.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class Humain
+{
+private:
+	string nom;
+	string boissonFavorite;
+
+public:
+	Humain(const string _nom = "", const string _boissonFavorite = "eau");
+	void sePresente();
+	void setBoissonFavorite(const string bF);
+	void boit();
+	string getNom();
+	string getBoissonFavorite();
+
+};
+
